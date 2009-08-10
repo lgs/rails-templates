8467f9e13504a412391e6ad3b800f84a5e698ab4
Luca G. Soave <luca.soave@gmail.com>
2009-08-10 17:27:25 +0000

    README cosmetics

--- test/config/routes.rb
+++
@@ -1,7 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resource :account, :controller => "users"
-  map.resources :password_resets
-  map.resources :users
-  map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
-end
